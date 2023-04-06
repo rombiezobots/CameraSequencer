a0e5df72228d7884398dfada55550c7c16039d73
Sam Van Hulle <hello@samvanhulle.com>
2023-04-06 16:30:16 +0200

    code cleanup

--- __init__.py
+++ __init__.py
@@ -1,63 +1,46 @@
-###############################################################################
-#                               CAMERA SEQUENCER                              #
-#         Shot-based user interface for cameras and timeline markers          #
-###############################################################################
-
-
-###############################################################################
+########################################################################################################################
 # Imports
-###############################################################################
+########################################################################################################################
 
 
-import bpy
-if 'properties' in locals():
+if 'bpy' in locals():
     import importlib
+
     properties = importlib.reload(properties)
     operators = importlib.reload(operators)
     panels = importlib.reload(panels)
 else:
-    from . import properties, operators, panels
+    import bpy
+    from . import properties
+    from . import operators
+    from . import panels
 
 
-###############################################################################
+########################################################################################################################
 # Add-on information
-###############################################################################
+########################################################################################################################
 
 
 bl_info = {
-    'name': 'Camera Sequencer',
-    'description': 'Shot-based user interface for cameras and timeline markers',
     'author': 'Sam Van Hulle',
-    'version': (0, 0, 1),
-    'blender': (2, 92, 0),
+    'blender': (3, 5, 0),
+    'category': 'Camera',
+    'description': 'Shot-based user interface for cameras and timeline markers',
     'location': 'Properties > Scene',
-    'category': 'Camera'
+    'name': 'Camera Sequencer',
+    'wiki_url': 'https://rombiezobots.com',
 }
 
 
-###############################################################################
-# Add-on preferences
-###############################################################################
-
-
-class CameraSequencerAddonPreferences(bpy.types.AddonPreferences):
-    bl_idname = __name__
-
-
-###############################################################################
+########################################################################################################################
 # Registration
-###############################################################################
+########################################################################################################################
 
 
-modules = [
-    properties,
-    operators,
-    panels
-]
+modules = [properties, operators, panels]
 
 
 def register():
-    bpy.utils.register_class(CameraSequencerAddonPreferences)
     for mod in modules:
         mod.register()
 
@@ -65,7 +48,6 @@ def register():
 def unregister():
     for mod in modules:
         mod.unregister()
-    bpy.utils.unregister_class(CameraSequencerAddonPreferences)
 
 
 if __name__ == '__main__':
